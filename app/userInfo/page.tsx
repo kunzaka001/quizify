e6b42dbbd8e2fefabc08b047a6b5ab2d4c161224
kunzaka001 <kunnoppakun007@gmail.com>
2024-09-10 20:57:35 +0700

    Updated userInfo Page

--- app/userInfo/page.tsx
+++ app/userInfo/page.tsx
@@ -30,14 +30,31 @@ function UserInfo() {
     }
   };
 
+  const handleBack = () => {
+    try {
+      router.push("/home");
+    } catch (error: any) {
+      console.error("Error signing out:", error.message);
+    }
+  };
+
   return (
     <div className="">
       <div className="">
         <h1 className="">
           This is Dashboard, {user ? user.displayName : "Guest"}!
         </h1>
-        <button onClick={handleLogout} className="">
+        <button
+          onClick={handleLogout}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+        >
           Logout
+        </button>
+        <button
+          onClick={handleBack}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+        >
+          Back
         </button>
       </div>
     </div>
