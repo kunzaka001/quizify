76d959d88d7df85aee7149abb158df60ffa1bd2a
kunzaka001 <137798282+kunzaka001@users.noreply.github.com>
2024-11-07 20:31:26 +0700

    [IN DEV] Add Features Section

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,15 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import quizifyLogo from "./assets/q.png";
 
+import {
+  BarChartHorizontal,
+  BatteryCharging,
+  CircleHelp,
+  Layers,
+  WandSparkles,
+  ZoomIn,
+} from 'lucide-react';
+
 const Landing = () => {
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
@@ -79,6 +88,45 @@ const Landing = () => {
     }
   };
 
+  const reasons = [
+    {
+      title: 'Quality',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?',
+      icon: <ZoomIn className="size-6" />,
+    },
+    {
+      title: 'Experience',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?',
+      icon: <BarChartHorizontal className="size-6" />,
+    },
+    {
+      title: 'Support',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?',
+      icon: <CircleHelp className="size-6" />,
+    },
+    {
+      title: 'Innovation',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?',
+      icon: <WandSparkles className="size-6" />,
+    },
+    {
+      title: 'Results',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?',
+      icon: <Layers className="size-6" />,
+    },
+    {
+      title: 'Efficiency',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe est aliquid exercitationem, quos explicabo repellat?',
+      icon: <BatteryCharging className="size-6" />,
+    },
+  ];
+
   return (
     <>
       <section className="flex justify-center items-center py-32">
@@ -115,6 +163,27 @@ const Landing = () => {
           </div>
         </div>
       </section>
+
+      <section className="flex justify-center items-center py-32">
+      <div className="container">
+        <div className="mb-10 md:mb-20">
+          <h2 className="mb-2 text-center text-3xl font-semibold lg:text-5xl">
+            Why Work With Us?
+          </h2>
+        </div>
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+          {reasons.map((reason, i) => (
+            <div key={i} className="flex flex-col">
+              <div className="mb-5 flex size-16 items-center justify-center rounded-full bg-accent">
+                {reason.icon}
+              </div>
+              <h3 className="mb-2 text-xl font-semibold">{reason.title}</h3>
+              <p className="text-muted-foreground">{reason.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
     </>
   );
 };
