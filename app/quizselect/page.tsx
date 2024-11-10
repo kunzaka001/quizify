e86006a5e4782f78374ccb0950db16fb29bebd2f
kunzaka001 <137798282+kunzaka001@users.noreply.github.com>
2024-11-10 15:44:46 +0700

    Create SelectQuiz page

---
+++ app/quizselect/page.tsx
@@ -0,0 +1,12 @@
+const quizSelect = () => {
+    return(
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <h1 className="">SELECT THINGS!!!</h1>
+            </div>
+        </>
+
+    )
+}
+
+export default quizSelect
